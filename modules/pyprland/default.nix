0ea458fe4864bb55b4984cbf8c674b1dcf81aa62
Aaron Danton <aarondanton04@hotmail.com>
2024-10-18 11:41:48 +0100

    Updated Pyprland and fixed nix spacing default to 2 spaces

--- modules/pyprland/default.nix
+++ modules/pyprland/default.nix
@@ -6,50 +6,50 @@ in {
     options.modules.pyprland = { enable = mkEnableOption "pyprland"; };
     config = mkIf cfg.enable {
         home.packages = with pkgs; [
-          pyprland
+            pyprland
         ];
 
         wayland.windowManager.hyprland.settings = {
             exec-once = [
-               "pypr"
+                "pypr"
             ];
 
             bind = [
-              "SUPER, T, exec, pypr toggle term"
-              "SUPER, P, exec, pypr toggle spotify"
+                "SUPER, T, exec, pypr toggle term"
+                "SUPER, P, exec, pypr toggle spotify"
             ];
         };
 
-xdg.configFile."hypr/pyprland.toml".source = (pkgs.formats.toml {}).generate "something" {
-          pyprland = {
-            plugins = [
-              "scratchpads"
-            ];
-          };
+        xdg.configFile."hypr/pyprland.toml".source = (pkgs.formats.toml {}).generate "something" {
+            pyprland = {
+                plugins = [
+                    "scratchpads"
+                ];
+            };
 
-          scratchpads.term = {
-            animation = "fromTop";
-            command = "alacritty --class Alacritty-dropdown-term";
-            class = "Alacritty-dropdown-term";
-            size = "75% 60%";
-            max_size = "1920px 100%";
-            margin = 50;
-            unfocus = "hide";
-            hysteresis = 0.4;
-            lazy = true;
-          };
+            scratchpads.term = {
+                animation = "fromTop";
+                command = "alacritty --class Alacritty-dropdown-term";
+                class = "Alacritty-dropdown-term";
+                size = "75% 60%";
+                max_size = "1920px 100%";
+                margin = 50;
+                unfocus = "hide";
+                hysteresis = 0.4;
+                lazy = true;
+            };
 
-          scratchpads.spotify = {
-            animation = "fromTop";
-            command = "alacritty --class Alacritty-dropdown-spotify -e spotify_player";
-            class = "Alacritty-dropdown-spotify";
-            size = "75% 60%";
-            max_size = "1920px 100%";
-            margin = 50;
-            unfocus = "hide";
-            hysteresis = 0.4;
-            lazy = true;
-          };
+            scratchpads.spotify = {
+                animation = "fromTop";
+                command = "alacritty --class Alacritty-dropdown-spotify -e spotify_player";
+                class = "Alacritty-dropdown-spotify";
+                size = "75% 60%";
+                max_size = "1920px 100%";
+                margin = 50;
+                unfocus = "hide";
+                hysteresis = 0.4;
+                lazy = true;
+            };
         };
     };
 }
