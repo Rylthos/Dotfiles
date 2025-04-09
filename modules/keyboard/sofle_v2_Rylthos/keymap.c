4ea8789583f151610d7650fb1b8909167e78d534
Aaron Danton <aarondanton04@hotmail.com>
2025-04-09 21:51:03 +0100

    Added more transparency keys

--- modules/keyboard/sofle_v2_Rylthos/keymap.c
+++ modules/keyboard/sofle_v2_Rylthos/keymap.c
@@ -17,7 +17,7 @@ enum LAYERS { _BASE, _SYMBOL, _ARROW, _RGB, _GAME };
 const key_override_t home_left_arrow = ko_make_with_layers(MOD_MASK_CTRL, KC_LEFT, KC_HOME, 1 << _ARROW);
 const key_override_t end_right_arrow = ko_make_with_layers(MOD_MASK_CTRL, KC_RGHT, KC_END, 1 << _ARROW);
 const key_override_t pagedn_down_arrow = ko_make_with_layers(MOD_MASK_CTRL, KC_DOWN, KC_PGDN, 1 << _ARROW);
-const key_override_t pageup_up_arrow = ko_make_with_layers(MOD_MASK_CTRL, KC_UP, KC_END, 1 << _ARROW);
+const key_override_t pageup_up_arrow = ko_make_with_layers(MOD_MASK_CTRL, KC_UP, KC_PGUP, 1 << _ARROW);
 
 const key_override_t* key_overrides[] = {
     &home_left_arrow,
@@ -63,9 +63,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_ARROW] = LAYOUT(
            KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                             KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-           KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                           MS_LEFT, MS_DOWN,   MS_UP, MS_RGHT,   KC_NO,   KC_NO,
-           KC_NO,   KC_NO, KC_VOLD, KC_MUTE, KC_VOLU,   KC_NO,                           KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT,   KC_NO,   KC_NO,
-         KC_TRNS,   KC_NO, KC_MPRV, KC_MPLY, KC_MNXT,   KC_NO,   KC_NO,           KC_NO, MS_BTN1, MS_WHLD, MS_WHLU, MS_BTN2,   KC_NO, KC_TRNS,
+           KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                           MS_LEFT, MS_DOWN,   MS_UP, MS_RGHT, KC_PSCR,   KC_NO,
+           KC_NO,   KC_NO, KC_VOLD, KC_MUTE, KC_VOLU,   KC_NO,                           KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, KC_SCRL,   KC_NO,
+         KC_TRNS,   KC_NO, KC_MPRV, KC_MPLY, KC_MNXT,   KC_NO, KC_TRNS,         KC_TRNS, MS_BTN1, MS_WHLD, MS_WHLU, MS_BTN2, KC_PAUS, KC_TRNS,
                            KC_TRNS, KC_TRNS, KC_TRNS,MO(_RGB), KC_TRNS,         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     ),
 
@@ -73,19 +73,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        TO(_GAME),   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                             KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
            KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                             KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
            KC_NO, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI,                             KC_NO, KC_VOLD, KC_MUTE, KC_VOLU,   KC_NO,   KC_NO,
-           KC_NO, RGB_TOG, RGB_HUD, RGB_SAD, RGB_VAD, RGB_SPD,   KC_NO,           KC_NO,   KC_NO, KC_MPRV, KC_MPLY, KC_MNXT,   KC_NO,   KC_NO,
-                             KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,           KC_NO, KC_TRNS,   KC_NO,   KC_NO, KC_NO
+           KC_NO, RGB_TOG, RGB_HUD, RGB_SAD, RGB_VAD, RGB_SPD, KC_TRNS,         KC_TRNS,   KC_NO, KC_MPRV, KC_MPLY, KC_MNXT,   KC_NO,   KC_NO,
+                           KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     ),
 
     [_GAME] = LAYOUT(
           KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                              KC_6,    KC_7,    KC_8,    KC_9,    KC_0,TO(_BASE),
-          KC_TAB,    KC_T,    KC_Q,    KC_W,    KC_E,    KC_R,                              KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_MINS,
+          KC_TAB,  KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,                              KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_MINS,
          KC_LSFT,    KC_A,    KC_A,    KC_S,    KC_D,    KC_F,                              KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_RSFT,
          KC_LSFT,    KC_B,    KC_Z,    KC_X,    KC_C,    KC_V, KC_MUTE,         KC_MPLY,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_ENT,
-                           KC_LALT, KC_LCTL, KC_LGUI, KC_LCTL,  KC_SPC,         KC_BSPC,   KC_NO,KC_RGUI,KC_RCTL, KC_RALT
+                           KC_LALT, KC_LCTL, KC_LGUI, KC_LCTL,  KC_SPC,         KC_BSPC,   KC_NO, KC_RGUI, KC_RCTL, KC_RALT
     ),
-    // clang-format on
 };
+// clang-format on
 
 void keyboard_pre_init_user(void)
 {
