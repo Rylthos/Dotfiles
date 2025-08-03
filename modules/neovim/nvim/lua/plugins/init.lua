6cec744930986a3e3a9520b836e69fede44e82b5
Aaron Danton <aarondanton04@hotmail.com>
2025-08-03 11:25:01 +0100

    Added Neogit

--- modules/neovim/nvim/lua/plugins/init.lua
+++ modules/neovim/nvim/lua/plugins/init.lua
@@ -1,129 +1,129 @@
 return {
-	{
-		"ibhagwan/fzf-lua",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-	},
+    {
+        "ibhagwan/fzf-lua",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+    },
 
-	-- LSP
-	{
-		"neovim/nvim-lspconfig",
-		dependencies = { 'saghen/blink.cmp' },
-	},
-	{
-		"mfussenegger/nvim-dap",
-		dependencies = {
-			"rcarriga/nvim-dap-ui",
-			"nvim-neotest/nvim-nio",
-		},
-	},
+    -- LSP
+    {
+        "neovim/nvim-lspconfig",
+        dependencies = { 'saghen/blink.cmp' },
+    },
+    {
+        "mfussenegger/nvim-dap",
+        dependencies = {
+            "rcarriga/nvim-dap-ui",
+            "nvim-neotest/nvim-nio",
+        },
+    },
 
-	{
-		'saghen/blink.cmp',
-		version = 'v0.*',
-		build = "cargo build --release",
-	},
+    {
+        'saghen/blink.cmp',
+        version = 'v0.*',
+        build = "cargo build --release",
+    },
 
-	"simrat39/rust-tools.nvim",
+    "simrat39/rust-tools.nvim",
 
-	"ray-x/lsp_signature.nvim",
+    "ray-x/lsp_signature.nvim",
 
-	"ThePrimeagen/vim-be-good",
+    "ThePrimeagen/vim-be-good",
 
-	"L3MON4D3/LuaSnip",
-	"rafamadriz/friendly-snippets",
-	"https://git.sr.ht/~whynothugo/lsp_lines.nvim",
-	"stevearc/conform.nvim",
+    "L3MON4D3/LuaSnip",
+    "rafamadriz/friendly-snippets",
+    "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+    "stevearc/conform.nvim",
 
-	{
-		"kevinhwang91/nvim-ufo",
-		dependencies = { "kevinhwang91/promise-async" }
-	},
+    {
+        "kevinhwang91/nvim-ufo",
+        dependencies = { "kevinhwang91/promise-async" }
+    },
 
-	{
-		"nvim-treesitter/nvim-treesitter",
-		main = "nvim-treesitter.configs",
-	},
+    {
+        "nvim-treesitter/nvim-treesitter",
+        main = "nvim-treesitter.configs",
+    },
 
-	"tpope/vim-sleuth",
+    "tpope/vim-sleuth",
 
-	"nvim-treesitter/playground",
+    "nvim-treesitter/playground",
 
-	"mbbill/undotree",
+    "mbbill/undotree",
 
-	"nvim-tree/nvim-web-devicons",
+    "nvim-tree/nvim-web-devicons",
 
-	"norcalli/nvim-colorizer.lua",
+    "norcalli/nvim-colorizer.lua",
 
-	"sindrets/diffview.nvim",
+    "sindrets/diffview.nvim",
 
-	{
-		"nvim-lualine/lualine.nvim",
-		dependencies = {
-			"nvim-tree/nvim-web-devicons",
-		},
-		enabled = true,
-	},
+    {
+        "nvim-lualine/lualine.nvim",
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
+        },
+        enabled = true,
+    },
 
-	{
-		"jake-stewart/multicursor.nvim",
-		branch = "1.0",
-	},
+    {
+        "jake-stewart/multicursor.nvim",
+        branch = "1.0",
+    },
 
-	"echasnovski/mini.comment",
+    "echasnovski/mini.comment",
 
-	"echasnovski/mini.move",
+    "echasnovski/mini.move",
 
-	-- "echasnovski/mini.pairs",
+    -- "echasnovski/mini.pairs",
 
-	"derekwyatt/vim-fswitch",
+    "derekwyatt/vim-fswitch",
 
-	{
-		"nvim-neo-tree/neo-tree.nvim",
-		branch = "v3.x",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-			"MunifTanjim/nui.nvim",
-			-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-		}
-	},
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+            "MunifTanjim/nui.nvim",
+            -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+        }
+    },
 
-	{
-		"m4xshen/hardtime.nvim",
-		dependencies = { "MunifTanjim/nui.nvim" },
-		opts = { },
-	},
+    {
+        "m4xshen/hardtime.nvim",
+        dependencies = { "MunifTanjim/nui.nvim" },
+        opts = { },
+    },
 
-	"kalvinpearce/ShaderHighlight",
+    "kalvinpearce/ShaderHighlight",
 
-	"chentoast/marks.nvim",
+    "chentoast/marks.nvim",
 
-	{
-		"iamcco/markdown-preview.nvim",
-		build = function()
-			vim.fn["mkdp#util#install"]()
-		end,
-	},
+    {
+        "iamcco/markdown-preview.nvim",
+        build = function()
+            vim.fn["mkdp#util#install"]()
+        end,
+    },
 
-	-- {
-	-- 	"christoomey/vim-tmux-navigator",
-	-- 	lazy = false,
-	-- },
+    -- {
+    -- 	"christoomey/vim-tmux-navigator",
+    -- 	lazy = false,
+    -- },
 
-	{
-		"lervag/vimtex",
-		lazy = false,
-	},
+    {
+        "lervag/vimtex",
+        lazy = false,
+    },
 
-	{
-		"stevearc/vim-arduino",
-		lazy = true,
-	},
+    {
+        "stevearc/vim-arduino",
+        lazy = true,
+    },
 
-	{
-		"kylechui/nvim-surround",
-		version = "*",
-	},
+    {
+        "kylechui/nvim-surround",
+        version = "*",
+    },
     "windwp/nvim-ts-autotag",
     {
         'windwp/nvim-autopairs',
@@ -134,61 +134,73 @@ return {
     "xiyaowong/transparent.nvim",
 
     {
-	    "ngtuonghy/live-server-nvim",
-	    lazy = true,
+        "ngtuonghy/live-server-nvim",
+        lazy = true,
     },
 
     {
-	    "f-person/git-blame.nvim",
-	    event = "VeryLazy",
+        "f-person/git-blame.nvim",
+        event = "VeryLazy",
 
-	    enabled = true,  -- if you want to enable the plugin
-	    message_template = " <summary> • <date> • <author>", -- template for the blame message, check the Message template section for more options
-	    date_format = "%m-%d-%Y %H:%M:%S", -- template for the date, check Date format section for more options
-	    virtual_text_column = 1,
+        enabled = true,  -- if you want to enable the plugin
+        message_template = " <summary> • <date> • <author>", -- template for the blame message, check the Message template section for more options
+        date_format = "%m-%d-%Y %H:%M:%S", -- template for the date, check Date format section for more options
+        virtual_text_column = 1,
     },
 
-	-- Themes
-	{
-		"scottmckendry/cyberdream.nvim",
-		lazy=false,
-		priority=1000,
-		config = function()
-			vim.cmd("colorscheme cyberdream")
-		end,
-		opts = {
-			transparent = true,
-			saturation = 0.8,
-		}
-	},
-	-- {
-	--   "akinsho/horizon.nvim",
-	--   opts = {
-	--       plugins =  {
-	-- 	cmp = true,
-	-- 	neo_tree = true,
-	-- 	gitsigns = true,
-	--       },
-	--   },
-	--   config = function()
-	-- 	  vim.cmd("colorscheme horizon")
-	--   end
-	-- },
-	-- "folke/tokyonight.nvim",
-	-- "EdenEast/nightfox.nvim",
-	-- {
-	-- 	"rebelot/kanagawa.nvim",
-	-- 	config = function()
-	-- 		vim.cmd("colorscheme kanagawa-wave")
-	-- 	end
-	-- },
+    {
+        "NeogitOrg/neogit",
+        dependencies = {
+            "nvim-lua/plenary.nvim",         -- required
+            "sindrets/diffview.nvim",        -- optional - Diff integration
+            "ibhagwan/fzf-lua",              -- optional
+        },
+    },
 
-	-- {
-	-- 	"ficcdaf/ashen.nvim",
-	-- 	lazy =false,
-	-- 	priority=1000,
-	-- 	-- config = function()
-	-- 	-- 	vim.cmd("colorscheme ashen")
-	-- 	-- end
-	-- },
+    -- Themes
+    {
+        "scottmckendry/cyberdream.nvim",
+        lazy=false,
+        priority=1000,
+        config = function()
+            vim.cmd("colorscheme cyberdream")
+        end,
+        opts = {
+            transparent = true,
+            saturation = 0.8,
+        }
+    },
+
+    --[[
+    {
+        "akinsho/horizon.nvim",
+        opts = {
+            plugins =  {
+                cmp = true,
+                neo_tree = true,
+                gitsigns = true,
+            },
+        },
+        config = function()
+            vim.cmd("colorscheme horizon")
+        end
+    },
+    "folke/tokyonight.nvim",
+    "EdenEast/nightfox.nvim",
+    {
+        "rebelot/kanagawa.nvim",
+        config = function()
+            vim.cmd("colorscheme kanagawa-wave")
+        end
+    },
+
+    {
+        "ficcdaf/ashen.nvim",
+        lazy =false,
+        priority=1000,
+        config = function()
+            vim.cmd("colorscheme ashen")
+        end
+    },
+    --]]
 }
