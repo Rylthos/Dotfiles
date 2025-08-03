9a7b84a4ab053b7fbcf8adbf9cdd103cd27a89d5
Aaron Danton <aarondanton04@hotmail.com>
2025-08-03 12:44:12 +0100

    Fixed screen-recorder

--- modules/screen_recorder/start_replay.sh
+++ modules/screen_recorder/start_replay.sh
@@ -6,6 +6,5 @@ mkdir -p "$video_path"
 
 # 60 FPS
 # cbr - constant bit rate
-# Two audio channels, output and input
 notify-send "Start replay"
-gpu-screen-recorder -w screen -f 60 -a "output/app-inverse:alsa_playback.spotify_player" -a "microphone/default_input" -a "spotify/app:alsa_playback.spotify_player" -c mkv -bm cbr -q 40000 -r 120 -o "$video_path"
+gpu-screen-recorder -w screen -f 60 -a "app-inverse:alsa_playback.spotify_player" -a "default_input" -a "app:alsa_playback.spotify_player" -c mkv -bm cbr -q 40000 -r 120 -o "$video_path"
