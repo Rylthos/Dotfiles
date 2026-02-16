cb306893c7e430226a4b44178d49add8fe75da4c
Aaron Danton <aarondanton04@hotmail.com>
2026-02-16 12:32:36 +0000

    add ssh-tokens for github-cam

--- modules/ssh/default.nix
+++ modules/ssh/default.nix
@@ -16,6 +16,14 @@ in {
                     user = "git";
                     identitiesOnly = true;
                 };
+                "github-cam" = {
+                    identityFile = "~/.ssh/id_github_cam";
+                    addKeysToAgent = "yes";
+                    hostname = "github.com";
+                    host = "github-cam";
+                    user = "git";
+                    identitiesOnly = true;
+                };
                 "gitlab.com" = {
                     identityFile = "~/.ssh/id_gitlab_cam";
                     addKeysToAgent = "yes";
