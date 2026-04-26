c688014a42dabdfc8f369bb6ec92fc4b72317700
Aaron Danton <aarondanton04@hotmail.com>
2026-04-26 20:27:05 +0100

    Add default styling to wofi

--- modules/wofi/default.nix
+++ modules/wofi/default.nix
@@ -6,6 +6,55 @@ in {
     config = mkIf cfg.enable {
         programs.wofi = {
             enable = true;
+            settings = {
+                location = "center";
+                width = 800;
+                height = 600;
+                sort = "alphabetical";
+                no_actions = true;
+            };
+            style = ''
+window {
+    margin: 0px;
+    border: 1px solid #${config.colorScheme.palette.base02};
+    background-color: #${config.colorScheme.palette.base00};
+}
+
+#input {
+    margin: 5px;
+    border: none;
+    color: #${config.colorScheme.palette.base05};
+    background-color: #${config.colorScheme.palette.base03};
+}
+
+#inner-box {
+    margin: 5px;
+    border: none;
+    background-color: #${config.colorScheme.palette.base03};
+}
+
+#outer-box {
+    margin: 5px;
+    border: none;
+    background-color: #${config.colorScheme.palette.base02};
+}
+
+#scroll {
+    margin: 0px;
+    padding-bottom: 5px;
+    border: none;
+}
+
+#text {
+    margin: 5px;
+    border: none;
+    color: #${config.colorScheme.palette.base05};
+}
+
+#entry:selected {
+    background-color: #${config.colorScheme.palette.base04};
+}
+            '';
         };
     };
 }
