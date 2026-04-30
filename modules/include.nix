accf8c14d3220be614d0d5d8dcce8a396a92bfb2
Aaron Danton <aarondanton04@hotmail.com>
2026-04-30 23:49:31 +0100

    config: report non-existant module names when including modules

--- modules/include.nix
+++ modules/include.nix
@@ -1,12 +1,23 @@
-{ inputs, ... }: {
+{ inputs, lib, ... }: {
   flake.lib = {
     loadNixosAndHM =
     config: modules: username:
     assert builtins.isAttrs config;
     assert builtins.isList modules;
     assert builtins.isString username;
-    {
-      imports = (builtins.map (module: config.flake.modules.nixos.${module} or {}) modules) ++ [
+    let
+      mapNixos = modules: (builtins.map (module: config.flake.modules.nixos.${module} or {}) modules);
+      mapHM = modules: (builtins.map (module: config.flake.modules.homeManager.${module} or {}) modules);
+    in {
+      assertions = [
+        {
+          assertion = builtins.all (name: (config.flake.modules.nixos ? ${name}) || (config.flake.modules.homeManager ? ${name})) modules;
+          message = let
+            missingNames = builtins.filter (name: !((config.flake.modules.nixos ? ${name}) || (config.flake.modules.homeManager ? ${name}))) modules;
+          in "Unknown modules: '${lib.concatStringsSep ", " missingNames}'";
+        }
+      ];
+      imports = (mapNixos modules) ++ [
         {
           imports = [ inputs.home-manager.nixosModules.home-manager ];
 
