84757294cf5c0968b920ad328cad8d92b1315304
Aaron Danton <aarondanton04@hotmail.com>
2026-04-30 23:51:21 +0100

    config: add nix-colors for colour scheme management

--- modules/parts.nix
+++ modules/parts.nix
@@ -2,6 +2,9 @@
   imports = [
     inputs.home-manager.flakeModules.home-manager
     inputs.flake-parts.flakeModules.modules
+
+    inputs.nix-colors.homeManagerModules.default
+    ({ ... }: {colorScheme = inputs.nix-colors.colorSchemes.catppuccin-macchiato; })
   ];
 
   config.systems = [
