f35c31022bc224d9d37afaf72490228a63269d5f
Aaron Danton <aarondanton04@hotmail.com>
2026-05-09 15:45:23 +0100

    config: add more options

--- modules/options.nix
+++ modules/options.nix
@@ -1,11 +1,34 @@
-{ self, lib, ... }: {
+{ self, lib, pkgs, ... }: {
   options.configuration = {
     machine.hostname = lib.mkOption { type = lib.types.str; };
 
     machine.host = lib.mkOption {
       type = lib.types.enum [
         "vm"
+        "desktop"
+        "laptop"
       ];
+    };
+
+    user = lib.mkOption { type = lib.types.str; };
+
+    muteOnLock = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      description = "Whether to enable muteOnLock";
+      example = true;
+    };
+
+    enableScreenRecorder = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+    };
+
+    lockScript = lib.mkOption {
+      type = lib.types.package;
+      default = pkgs.writeShellScriptBin "lock" ''
+        inotify "Unimplemented Lock script"
+      '';
     };
   };
 }
